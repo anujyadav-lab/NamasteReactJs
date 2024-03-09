f42115419d51c781da8c1dbdcb7df7e1b19f5057
Anuj Yadav <ay517328@gmail.com>
2024-03-09 18:23:26 +0530

    episode-1 part-3

---
+++ App.js
@@ -0,0 +1,13 @@
+const headingElement = React.createElement('h1',{id:"h1ID"},'har har mahadev');
+    console.log(headingElement)// gives object.
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
+
+root.render(headingElement)
+
+
+
+
+ 
