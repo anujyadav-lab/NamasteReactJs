40df82526c89307bb2c337e874f8e0583dc7ba92
Anuj Yadav <ay517328@gmail.com>
2024-03-11 14:04:08 +0530

    completed

--- App.js
+++ App.js
@@ -1,47 +1,48 @@
 
 // This parent React element is actually a object and this react object becomes html that the browser understand.
 const parent = React.createElement(
-'div',
-{id:'parent'},
-
-// React.createElement('div',{id:'child'},React.createElement('h1',{},'i am h1 tag'),React.createElement('h1',{},'i am h1 tag')));
-
-React.createElement('div',{id:'child'},
-[
-    React.createElement('h1',{},'i am h1 tag'),
-    React.createElement('h2',{},'i am h2 tag'),
-
-]),
-React.createElement('div',{id:'child'},
-[
-    React.createElement('h1',{},'i am h1 tag'),
-    React.createElement('h2',{},'i am h2 tag'),
-
-]
-));
-
-
-
-
-console.log(parent)
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-
-
-root.render(parent);//while it is rendering onto the DOM it converts itself into that html and puts it up into the DOM.
-
-// const headingElement = React.createElement('h1',{id:"h1ID"},'har har mahadev');
-//     console.log(headingElement)// gives object.
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
-
-
-
-// root.render(headingElement)
- 
-
-
-
-
-
- 
+    'div',
+    {id:'parent'},
+    
+    // React.createElement('div',{id:'child'},React.createElement('h1',{},'i am h1 tag'),React.createElement('h1',{},'i am h1 tag')));
+    
+    React.createElement('div',{id:'child'},
+    [
+        React.createElement('h1',{},'i am h1 tag'),
+        React.createElement('h2',{},'i am h2 tag'),
+    
+    ]),
+    React.createElement('div',{id:'child'},
+    [
+        React.createElement('h1',{},'i am h1 tag'),
+        React.createElement('h2',{},'i am h2 tag'),
+    
+    ]
+    ));
+    
+    
+    
+    
+    
+    // console.log(parent)
+    const root = ReactDOM.createRoot(document.getElementById('root'))
+    
+    
+    
+    root.render(parent);//while it is rendering onto the DOM it converts itself into that html and puts it up into the DOM.
+    
+    // const headingElement = React.createElement('h1',{id:"h1ID"},'har har mahadev');
+    //     console.log(headingElement)// gives object.
+    // const root = ReactDOM.createRoot(document.getElementById('root'));
+    
+    
+    
+    
+    // root.render(headingElement)
+     
+    
+    
+    
+    
+    
+     
