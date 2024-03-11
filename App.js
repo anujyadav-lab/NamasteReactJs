035ccf17ba612ae77f0e44e04d799b4704a11ec8
Anuj Yadav <ay517328@gmail.com>
2024-03-11 19:59:55 +0530

    completed2

--- App.js
+++ App.js
@@ -12,19 +12,23 @@ const parent = React.createElement(
         React.createElement('h2',{},'i am h2 tag'),
     
     ]),
-    React.createElement('div',{id:'child'},
+    React.createElement('div',{id:'child2'},
     [
         React.createElement('h1',{},'i am h1 tag'),
         React.createElement('h2',{},'i am h2 tag'),
     
     ]
     ));
+
     
     
-    
-    
-    
+    // parent = {
+
+    // }
+
+    // <parent>
     // console.log(parent)
+
     const root = ReactDOM.createRoot(document.getElementById('root'))
     
     
