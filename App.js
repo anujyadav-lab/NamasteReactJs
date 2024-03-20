7c6a97a0ba3ca0ecc15e43a335c65f1982bed846
Anuj Yadav <ay517328@gmail.com>
2024-03-20 10:31:37 +0530

    epi-3 part-2

--- App.js
+++ App.js
@@ -1,41 +1,29 @@
-import React from "react";
+import React, { Children } from "react";
 import ReactDOM from 'react-dom/client'
 
-const parent = React.createElement(
-    'div',
-    {id:'parent'},
-    
-    
-    React.createElement('div',{id:'child'},
-    [
-        React.createElement('h1',{},'Namaste React 🚀'),
-        React.createElement('h2',{},'i am h2 tag'),
-    
-    ]),
-    React.createElement('div',{id:'child2'},
-    [
-        React.createElement('h1',{},'i am h1 tag'),
-        React.createElement('h2',{},'i am h2 tag'),
-    
-    ]
-    ));
+// React.createElement -> object -> HTMLElement(render on dom).
 
+const heading = React.createElement(
     
-    
+    "h1",
 
+    {id:'heading'},
 
-    const root = ReactDOM.createRoot(document.getElementById('root'))
-    
-    
-    
-    root.render(parent);
+    "Namaste React 🚀🚀🚀"
 
+    )
     
- 
+    console.log(heading)
     
-    
-    
+// const root = ReactDOM.createRoot('root') made mistake here.
+// root.render(heading);
      
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')); //This root is the root for our react app,so whatever happens in react that will happen in this root,suppose if i want to render something that will render inside the root.
+console.log(root)
+
+root.render(heading);
     
     
     
