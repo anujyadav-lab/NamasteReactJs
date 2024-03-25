9966e77901ee0376d75a873dd2db9dc0956c8313
Anuj Yadav <ay517328@gmail.com>
2024-03-25 12:06:59 +0530

    ep-3 part-4

--- App.js
+++ App.js
@@ -3,27 +3,36 @@ import ReactDOM from 'react-dom/client'
 
 // React.createElement -> object -> HTMLElement(render on dom).
 
-const heading = React.createElement( //In the core of react this how we create a ract element.But as it is not developer friendly we use jsx to create react elements.
+const Title = () => (  //In the core of react this how we create a ract element.But as it is not developer friendly we use jsx to create react elements.
     
-    "h1",
+    <h1 className="head">
+        Namaste React Using Jsx.
+    </h1>
 
-    {id:'heading'},
+    );
+ 
+
+// React Functional component
+// const HeadingComponent = () =>{
+//      <h1>Namaste React Functional component </h1>
+// };
 
 
-    "Namaste React 🚀🚀🚀"
 
-    )
-    console.log(heading)
-    
-    const jsxHeading = <h1 id="heading">Namaste React Using Jsx </h1>;
+const HeadingComponent = () => (
+    <div id = "container">
+        <Title/>
+            <h1 className="heading">Namaste Reactjs</h1>
 
-    console.log(jsxHeading)
-// const root = ReactDOM.createRoot('root') made mistake here.
-// root.render(heading);
-     
+    </div>
+);
+
+
+
+
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root')); //This root is the root for our react app,so whatever happens in react that will happen in this root,suppose if i want to render something that will render inside the root.
 
-root.render(jsxHeading);
-
+root.render(<HeadingComponent/>);
