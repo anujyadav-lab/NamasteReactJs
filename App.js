2738ac7fe592c29f84e123cf3648a9626ff70292
Anuj Yadav <ay517328@gmail.com>
2024-03-26 09:56:23 +0530

    !

--- App.js
+++ App.js
@@ -1,36 +1,24 @@
 import React, { Children } from "react";
 import ReactDOM from 'react-dom/client'
 
-
-const Title =  () =>( 
-    
-    <h1 className="head">
-        Namaste React Using Jsx.
-    </h1>
-    
-
-    );
-
-
-// const data = <h1>alert('you are safe')</h1>
-
-const number = 1000;
-
-const HeadingComponent = () => (
-    <div id = "container">
-        {/* {data} */}
-{Title()}
-<Title/>
-<Title></Title>
-
-            <h1 className="heading">Namaste Reactjs</h1>
-
-    </div>
-);
+const Title = () => (
+   (
+<>
+  <div>
+    <h2> hi how are you!</h2>
+  </div>
+  
+   <div>
+   <h2>hi you!</h2>
+ </div>
+ </>
+  )
+)
 
 
-
-
+const HeadingComponent = () =>{
+ return   <bold><Title/></bold>
+}
 
 
 
