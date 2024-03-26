e87a0e471027e02590873f307e72b907d0a2beec
Anuj Yadav <ay517328@gmail.com>
2024-03-26 06:36:34 +0530

    Done

--- App.js
+++ App.js
@@ -1,49 +1,39 @@
 import React, { Children } from "react";
 import ReactDOM from 'react-dom/client'
 
-// React.createElement -> object -> HTMLElement(render on dom).
 
-// const Title = () => (  //In the core of react this how we create a react element.But as it is not developer friendly we use jsx to create react elements.
+const Title =  () =>( 
     
-//     <h1 className="head">
-//         Namaste React Using Jsx.
-//     </h1>
+    <h1 className="head">
+        Namaste React Using Jsx.
+    </h1>
+    
 
-//     );
- 
-
-// React Functional component
-// const HeadingComponent = () =>{
-//      <h1>Namaste React Functional component </h1>
-// };
+    );
 
 
+// const data = <h1>alert('you are safe')</h1>
 
-// const HeadingComponent = () => (
-//     <div id = "container">
-//         <Title/>
-//             <h1 className="heading">Namaste Reactjs</h1>
+const number = 1000;
 
-//     </div>
-// );
+const HeadingComponent = () => (
+    <div id = "container">
+        {/* {data} */}
+{Title()}
+<Title/>
+<Title></Title>
 
-const MyCareer = () =>{
-    return <h2>Uttam Developer</h2>;
-}
-
-const Container = () =>{
-    return <div>
-  <MyCareer/>
-  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut possimus quasi iusto quibusdam distinctio architecto adipisci, accusantium vero ea ratione!</p>
-  
-
+            <h1 className="heading">Namaste Reactjs</h1>
 
     </div>
-}
+);
 
 
 
 
-const root = ReactDOM.createRoot(document.getElementById('root')); //This root is the root for our react app,so whatever happens in react that will happen in this root,suppose if i want to render something that will render inside the root.
 
-root.render(<Container/>);
+
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+
+root.render(<HeadingComponent/>);
