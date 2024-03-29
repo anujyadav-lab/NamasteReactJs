d869a1c0af87869a2f665c2237ff6dd7c8a6a9c1
Anuj Yadav <ay517328@gmail.com>
2024-03-29 06:21:09 +0530

    epi-5 -part one

---
+++ CH-5-LetsGetHooked/src/components/Body.js
@@ -0,0 +1,28 @@
+import RestaurantCard from "./RestaurantCard";
+import resList from "../utils/MockData";
+
+
+
+
+ const Body = () => {
+    return (
+      <div className="body">
+   
+  
+       
+  
+        <div className="res-container">
+        {
+          resList.map(
+            (restaurant) =>{
+                 return  <RestaurantCard key={restaurant.id} resData = {restaurant}/>
+  
+            }
+  )
+        }
+        </div>
+      </div>
+    );
+  };
+  
+export default Body;
---
+++ CH-5-LetsGetHooked/src/components/Search.js
@@ -0,0 +1,10 @@
+
+const Search = () =>{
+    return(
+        <div className="search">
+        <input className="searchInput" type="text"></input>
+        <button id="btn-1"> Search</button>
+      </div>
+    )
+}
+export default Search;
